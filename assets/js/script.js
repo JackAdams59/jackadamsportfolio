d96ec1202d7a160035745677766c5b4259a2f49c
JackAdams59 <adamsj5@tcnj.edu>
2024-04-17 19:28:26 -0400

    a

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+function myFunction() {
+document.getElementById("submit").innerHTML = "Thank you. Your answer has been recorded."
+
+}
+console.log("hello");
+
+if(jQuery) {console.log ("jQuery Loaded")
+
+}
+else {
+    console.log ("no jQuery")
+}
